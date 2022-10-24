de752d1065fcf69d004dcc0536b88da5f50093e4
RyanMcG09 <74496559+RyanMcG09@users.noreply.github.com>
2022-10-24 15:56:35 +0100

    Add files via upload

---
+++ 20Core.py
@@ -0,0 +1,48 @@
+import math
+import time
+import multiprocessing
+from multiprocessing import Process
+from multiprocessing import Pool
+import matplotlib.pyplot as plt
+
+def Sleeping():
+  time.sleep(1)
+
+if __name__ == '__main__':
+  noOfCores = [2,4,6,8,10,12,14,16,18,20]
+  timeOfCores = [0,0,0,0,0,0,0,0,0,0]
+  pool = Pool()
+  count = 0
+  for n in noOfCores:
+    noOfIterations = math.ceil(20/n)
+    print('Number of Iterations is = {}'.format(noOfIterations))
+    start = time.perf_counter()
+    for i in range(noOfIterations):
+      for j in range(n):
+        result = pool.apply_async(Sleeping)
+      for j in range(n):
+        answer = result.get(timeout=10)
+    finish = time.perf_counter()
+    total = finish - start
+    timeOfCores[count] = total
+    print('the program has finished in {} seconds'.format(total))
+    count = count+1
+
+plt.figure(1)
+plt.plot(noOfCores, timeOfCores)
+plt.title("Execution Time against the Number of Cores Used")
+plt.xlabel('Number of Cores Used')
+plt.ylabel('Execution Time (s)')
+plt.xticks([2,4,6,8,10,12,14,16,18,20])
+plt.savefig('times.png')
+
+speedup = [0,0,0,0,0,0,0,0,0,0]
+for i in range(10):
+  speedup[i] = timeOfCores[0]/timeOfCores[i]
+plt.figure(2)
+plt.plot(noOfCores, speedup)
+plt.title("Speedup against the Number of Cores Used")
+plt.xlabel('Number of Cores Used')
+plt.ylabel('Speedup')
+plt.xticks([2,4,6,8,10,12,14,16,18,20])
+plt.savefig('speedup.png')
